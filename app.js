752fa78299b8cb2f780c0cd869f5d7058f4b8c93
Mario Villalobos <mariocr1611@gmail.com>
2020-09-30 19:05:28 -0600

    Show feedback input, show results

--- app.js
+++ app.js
@@ -8,18 +8,16 @@ $("#btn-go").click(function() {
   var email = document.getElementById("email").value;
   email = email.trim();
 
-  var validEmail = validEmails.some(function (element) {
+  var validEmail = validEmails.some(function(element) {
     return element === email;
-  })
+  });
 
-  if(validEmail){
+  if (validEmail) {
     sendValue(email);
     hideError();
-
   } else {
     showError();
   }
-
 });
 
 function init() {
@@ -28,15 +26,17 @@ function init() {
 
   for (let i = 0; i < numberOptions; i++) {
     showNumber--;
-    document.getElementById('options')
-    .insertAdjacentHTML(
-      'afterbegin', 
-      '<div class="col-md-6 col-lg-6 option"><div class="option-number"><div class="blue-circle"><span class="text-gold">'+ showNumber +'</span></div></div> <div class="text-option"><h2 class="text-blue">Lorem Ipsum</h2><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt ut labore et dolore magnaaliqua.</span> </div></div>'
-    )
-  };
+    document
+      .getElementById("options")
+      .insertAdjacentHTML(
+        "afterbegin",
+        '<div class="col-md-6 col-lg-6 option"><div class="option-number"><div class="blue-circle"><span class="text-gold">' +
+          showNumber +
+          '</span></div></div> <div class="text-option"><h2 class="text-blue">Lorem Ipsum</h2><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, seddo eiusmod tempor incididunt ut labore et dolore magnaaliqua.</span> </div></div>'
+      );
+  }
 
   searchInit();
-
 }
 
 function sendValue(email) {
@@ -59,47 +59,63 @@ function sendValue(email) {
 }
 
 function searchInit() {
-  document.getElementById('text-search').textContent = "Search Any Email Address";
-  document.getElementById('text-subtext').textContent = "Look up the owner's name,photos and online profiles. See what you find!";
-  document.getElementById('text-subtext-gold').textContent = "Start Here";
+  document.getElementById("text-search").textContent =
+    "Search Any Email Address";
+  document.getElementById("text-subtext").textContent =
+    "Look up the owner's name,photos and online profiles. See what you find!";
+  document.getElementById("text-subtext-gold").textContent = "Start Here";
 }
 
 function searchSuccess() {
-  document.getElementById('text-search').textContent = "Can’t Find The Right Person?";
-  document.getElementById('text-subtext').textContent = "Make a new search";
-  document.getElementById('text-subtext-gold').textContent = "Try Again";
+  document.getElementById("text-search").textContent =
+    "Can’t Find The Right Person?";
+  document.getElementById("text-subtext").textContent = "Make a new search";
+  document.getElementById("text-subtext-gold").textContent = "Try Again";
 }
 
 function showError() {
-  document.querySelector(".email").classList.add("error-border");
+  document.querySelector("#email").classList.add("error-border");
+  document.querySelector("#input-label").classList.add("error-label");
+  document.querySelector("#input-label").textContent= "Please add a valid email address";
 }
 
 function hideError() {
-  document.querySelector(".email").classList.remove("error-border");
+  document.querySelector("#email").classList.remove("error-border");
+  document.querySelector("#input-label").classList.remove("error-label");
+  document.querySelector("#input-label").textContent= "EMAIL";
 }
 
 function successSend(person) {
-  document.getElementById('phone-numbers').innerHTML = "";
-  document.getElementById('relatives').innerHTML = "";
   searchSuccess();
+  document.getElementById("phone-numbers").innerHTML = "";
+  document.getElementById("relatives").innerHTML = "";
 
   var fullName = person.first_name + " " + person.last_name;
-  document.getElementById('name-span').textContent = fullName;
-  document.getElementById('email-span').textContent = person.email;
-  document.getElementById('address-span').textContent = person.address;
-  document.getElementById('description-span').textContent = person.description;
-  document.getElementById('reverse-email').style.display="none";
+
+  document.getElementById("name-span").textContent = fullName;
+  document.getElementById("email-span").textContent = person.email;
+  document.getElementById("address-span").textContent = person.address;
+  document.getElementById("description-span").textContent = person.description;
+  document.getElementById("reverse-email").style.display = "none";
+  document.getElementById("result-container").style.display = "block";
 
   person.phone_numbers.forEach(function(element) {
-    document.getElementById('phone-numbers')
-    .insertAdjacentHTML('afterbegin', '<li class="text-blue number-list-li">'+element+'</li>')
+    document
+      .getElementById("phone-numbers")
+      .insertAdjacentHTML(
+        "afterbegin",
+        '<li class="text-blue number-list-li">' + element + "</li>"
+      );
   });
 
   person.relatives.forEach(function(element) {
-    document.getElementById('relatives')
-    .insertAdjacentHTML('afterbegin', '<li class="relatives-list-li">'+element+'</li>')
+    document
+      .getElementById("relatives")
+      .insertAdjacentHTML(
+        "afterbegin",
+        '<li class="relatives-list-li">' + element + "</li>"
+      );
   });
-
 }
 
 init();
